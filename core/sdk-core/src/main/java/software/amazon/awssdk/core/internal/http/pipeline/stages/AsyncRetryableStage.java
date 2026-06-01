895b569b2041f8da4570acc24e62a848a9c7c61d
Sudhanshu Sali <salisudhanshu@gmail.com>
2026-05-31 21:52:32 -0400

    Revert "Fix NPE in AsyncRetryableStage (#6936)"
    This reverts commit 5deef3caa541004958b6495f3d1ede4fbb79e334.

--- core/sdk-core/src/main/java/software/amazon/awssdk/core/internal/http/pipeline/stages/AsyncRetryableStage.java
+++ core/sdk-core/src/main/java/software/amazon/awssdk/core/internal/http/pipeline/stages/AsyncRetryableStage.java
@@ -192,10 +192,6 @@ public final class AsyncRetryableStage<OutputT> implements RequestPipeline<SdkHt
      * Returns the suggested backoff delay based on the 'x-amz-retry-after' header value in the response.
      */
     private Optional<Duration> xAmzRetryAfter(SdkHttpResponse response) {
-        if (response == null) {
-            return Optional.empty();
-        }
-
         Optional<String> optionalXAmzRetryAfter = response.firstMatchingHeader(X_AMZ_RETRY_AFTER_HEADER);
         return optionalXAmzRetryAfter.map(xAmzRetryAfter -> {
             try {
